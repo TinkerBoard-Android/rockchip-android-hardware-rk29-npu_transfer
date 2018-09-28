97f24cea75266c6b37d3d3ef88bff4d859ef0f8f
Weixin Zhou <zwx@rock-chips.com>
2018-09-28 17:37:13 +0800

    add npu transfer proxy service
    Change-Id: I5c7bed465bafa8a1d28fe68b6513dbf7690b2a19
    Signed-off-by: Weixin Zhou <zwx@rock-chips.com>
    Signed-off-by: ZengFei <felix.zeng@rock-chips.com>

---
+++ npu_transfer.mk
@@ -0,0 +1,3 @@
+PRODUCT_COPY_FILES += \
+		      hardware/rockchip/npu_transfer/bin/npu_transfer_proxy:vendor/bin/npu_transfer_proxy \
+		      hardware/rockchip/npu_transfer/npu_transfer_proxy.rc:vendor/etc/init/npu_transfer_proxy.rc \
