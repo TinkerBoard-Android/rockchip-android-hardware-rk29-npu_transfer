db3923ab1c54e4b0fc6330cb9ca02da492147aa0
Bian Jin chen <kenjc.bian@rock-chips.com>
2019-01-25 16:25:51 +0800

    sepolicy: remote selabel in android pie.
    Change-Id: I0de7193e26ef4f5165d9d43bf231c2c52a920e26
    Signed-off-by: Bian Jin chen <kenjc.bian@rock-chips.com>

--- npu_transfer.mk
+++ npu_transfer.mk
@@ -1,3 +1,9 @@
+ifeq (1,$(strip $(shell expr $(PLATFORM_VERSION) \>= 9)))
 PRODUCT_COPY_FILES += \
-		      hardware/rockchip/npu_transfer/bin/npu_transfer_proxy:vendor/bin/npu_transfer_proxy \
-		      hardware/rockchip/npu_transfer/npu_transfer_proxy.rc:vendor/etc/init/npu_transfer_proxy.rc \
+    hardware/rockchip/npu_transfer/npu_transfer_proxy_pie.rc:vendor/etc/init/npu_transfer_proxy.rc
+else
+PRODUCT_COPY_FILES += \
+    hardware/rockchip/npu_transfer/npu_transfer_proxy.rc:vendor/etc/init/npu_transfer_proxy.rc
+endif
+PRODUCT_COPY_FILES += \
+    hardware/rockchip/npu_transfer/bin/npu_transfer_proxy:vendor/bin/npu_transfer_proxy
