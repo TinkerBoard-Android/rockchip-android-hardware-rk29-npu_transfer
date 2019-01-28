9d201313c32c0feafd5bcef0662136a8266d153a
hzf <hzf@rock-chips.com>
2019-01-28 15:32:07 +0800

    npu_transfer: fix compilation errors on Android N.
    Change-Id: I54125db7baa8d2836b3da2e99e3b1fe373934c3b
    Signed-off-by: Bian Jinchen <kenjc.bian@rock-chips.com>

--- npu_transfer.mk
+++ npu_transfer.mk
@@ -1,9 +1,9 @@
 ifeq (1,$(strip $(shell expr $(PLATFORM_VERSION) \>= 9)))
 PRODUCT_COPY_FILES += \
-    hardware/rockchip/npu_transfer/npu_transfer_proxy_pie.rc:vendor/etc/init/npu_transfer_proxy.rc
+    hardware/rockchip/npu_transfer/npu_transfer_proxy_pie.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/npu_transfer_proxy.rc
 else
 PRODUCT_COPY_FILES += \
-    hardware/rockchip/npu_transfer/npu_transfer_proxy.rc:vendor/etc/init/npu_transfer_proxy.rc
+    hardware/rockchip/npu_transfer/npu_transfer_proxy.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/npu_transfer_proxy.rc
 endif
 PRODUCT_COPY_FILES += \
-    hardware/rockchip/npu_transfer/bin/npu_transfer_proxy:vendor/bin/npu_transfer_proxy
+    hardware/rockchip/npu_transfer/bin/npu_transfer_proxy:$(TARGET_COPY_OUT_VENDOR)/bin/npu_transfer_proxy
